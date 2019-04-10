7a2bee724537bb0f4f368aa4dd3c07190d547244
Scott Meade <scott@synapsoftware.com>
2019-04-10 15:13:55 -0600

    Programming Phoenix, Chapter 5: Authenticating Users - Writing an Authentication Plug - Implementing Login and Logout - Presenting User Account Links

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -18,6 +18,30 @@ defmodule Rumbl.Accounts do
     Repo.get_by(User, params)
   end
 
+  import Ecto.Query
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_pass(email, given_pass) do
+    user = get_user_by_email(email)
+
+    cond do
+      user && Comeonin.Pbkdf2.checkpw(given_pass, user.credential.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Comeonin.Pbkdf2.dummy_checkpw()
+        {:error, :not_found}
+    end
+  end
+
   def list_users do
     Repo.all(User)
   end
@@ -41,4 +65,5 @@ defmodule Rumbl.Accounts do
     |> User.registration_changeset(attrs)
     |> Repo.insert()
   end
+
 end
